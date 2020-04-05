6b197e29f7020bb44e2434fe97414826c3c3dd15
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-04-05 23:22:49 +0530

    refactor: Disable spread warning for the line

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '../public/reset.css';
 import '../public/style.css';
 
 function QriusApp({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
 
